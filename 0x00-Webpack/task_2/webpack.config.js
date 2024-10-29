1fb8ecb9b4cff6749df38f8fcfa6c27ac7dd0d42
josephlaryeanortey <christiangospelhype@gmail.com>
2024-10-29 04:34:50 +0000

    alx

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,34 +1,40 @@
+// webpack.config.js
+
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
-  performance: {
-    maxAssetSize: 1000000,
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+    mode: 'production',
+    entry: './js/dashboard_main.js', // Entry point
+    output: {
+        filename: 'bundle.js', // Output file name
+        path: path.resolve(__dirname, 'public'), // Output directory
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Transpile JS files
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
-          },
+            {
+                test: /\.css$/, // Handle CSS files
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/, // Optimize images
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]', // Preserve original file names
+                        outputPath: 'assets/', // Output images in a specific directory
+                    },
+                },
+            },
         ],
-      },
-    ],
-  },
+    },
 };
