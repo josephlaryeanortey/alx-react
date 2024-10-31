e656efb6ff4ef4e11dab9e55d6a9cd45c517e9d8
josephlaryeanortey <christiangospelhype@gmail.com>
2024-10-31 01:00:53 +0000

    react intro project

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.png'; // Assuming you named the logo file 'logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton logo" />
+        <h1>School dashboard</h1>
       </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
     </div>
   );
 }
