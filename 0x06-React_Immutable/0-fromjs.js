d2bddd0c653700a534f51e3aca95e6d6f2109559
josephlaryeanortey <christiangospelhype@gmail.com>
2024-11-21 22:41:23 +0000

    reactform

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,10 @@
 import { fromJS } from 'immutable';
 
+/**
+ * Converts a plain JavaScript object into an Immutable Map using fromJS.
+ * @param {Object} object - The plain JavaScript object to convert.
+ * @returns {Immutable.Map} - The resulting Immutable Map.
+ */
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
